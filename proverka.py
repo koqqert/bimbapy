3348f789b61f7598341d2e1249a3cb32a5de318d
koqqert <koqqert@gmail.com>
2024-04-24 20:47:52 +0500

    2 функции для проекта, пока что просто функции

--- proverka.py
+++ proverka.py
@@ -1,12 +1,29 @@
-def is_even_sum(func):
-    def wrapper(*args):
-        result = func(*args)
-        if result % 2 == 0:
-            return True
-        else: return False
-    return wrapper
-@is_even_sum
-def get_sum(*args):
-    return sum(args)
+import sqlite3
 
-print(get_sum(1, 2, 3, 4))
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS test(
+    name text,
+    age integer,
+    address text
+)
+""")
+
+date = 26.04       #обычные значения для проверки
+name_column = 26.04
+mark = 4
+name = "Mike"
+
+def add_column(date): #создать столбец в таблице
+    c.execute(f"""ALTER TABLE test
+            ADD COLUMN "{date}" integer NOT NULL DEFAULT 'нету'
+    """)
+    conn.commit()
+    
+def add_mark(name, name_column, mark): #добавлять значение в столбец
+    c.execute(f"""UPDATE test SET "{name_column}" = "{mark}" WHERE name = '{name}'
+    """)
+    conn.commit()
+
+conn.commit()
+# conn.close()
