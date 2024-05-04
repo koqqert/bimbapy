f2697a47ec5bff0747ba16e3a68f77265f78fcfa
koqqert <koqqert@gmail.com>
2024-05-04 22:22:41 +0500

    надо решить функцию

--- proverka.py
+++ proverka.py
@@ -11,7 +11,7 @@ c.execute("""CREATE TABLE IF NOT EXISTS journal(
 #обычные значения для проверки
 name_column = "02.09.24"
 rename_column = "03.09.24"
-mark = 4
+mark = 5
 name = "артем"
 name_id = 1
 
@@ -23,19 +23,24 @@ def add_column(name_column): #создать столбец в таблице
 def add_mark(name, name_column, mark): #добавлять значение в столбец
     c.execute(f"UPDATE journal SET '{name_column}' = '{mark}' WHERE id = '{name_id}'")
     conn.commit()
+    print(f"Вы успешно добавили пользователю {name} оценку {mark} на число {name_column}")
 # add_mark(name, name_column, mark)
 
 def get_mark_all(*args): #получить все значения из строки
     c.execute(f"SELECT * FROM journal WHERE id = '{name_id}'")
     marks = c.fetchone()[2:]
-    return print(''.join(map(str, marks)))
+    c.execute(f"SELECT name FROM journal WHERE id = '{name_id}'")
+    name = c.fetchall().pop()[0]
+    conn.commit()
+    return print(f"{name}: " + ''.join(map(str, marks)))
 # get_mark_all()
 
-def get_mark_day(name, name_column): #получить значение из столбца в опред. день
-    c.execute(f"SELECT '{name_column}' FROM journal WHERE id = '{name_id}'")
-    mark = c.fetchall().pop()[0]
-    return print(mark)
-# get_mark(name, name_column)
+def get_mark_day(name, name_column, name_id): #получить значение из столбца в опред. день
+    c.execute(f"SELECT '{name_column}' FROM journal WHERE name = '{name}' AND id = '{name_id}'")
+    mark = c.fetchone()[0]
+    conn.commit()
+    return print(f"Вы успешно получили оценку {mark} на число {name_column} пользователю {name}")
+get_mark_day(name, name_column, name_id)
 
 conn.commit()
 conn.close()
