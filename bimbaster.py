dd5202b6b5ce7b7a4e01e09936e455c584107b51
koqqert <koqqert@gmail.com>
2024-05-07 21:19:04 +0500

    homework

--- bimbaster.py
+++ bimbaster.py
@@ -1,19 +1,41 @@
 #homework
-def separator(sym='', count=0):
-    def decorator(func,):
-        def wrapper(*args, **kwargs):
-            print(sym * count)
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+import random
+import telebot
+bot = telebot.TeleBot("6862004066:AAHgMHlkpMh7qiXpqoHSpgYIbb9vE5jApCI")
 
-@separator('-', 20)
-def say_hello(name):
-    return f'Добрый день, {name}'
+facts = [
+    "Хамелеоны могут двигать глазами в разных направлениях одновременно.",
+    "Электрический угорь может вырабатывать энергию, способную зажечь 12 лампочек.",
+    "Морские котики могут задерживать дыхание на два часа.",
+]
 
-@separator('+', 30)
-def say_bye(name):
-    return f'До свидания, {name}'
+@bot.message_handler(commands=['start'])
+def start(message):
+    kbrd = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    button1 = telebot.types.KeyboardButton('Привет')
+    button2 = telebot.types.KeyboardButton('Как дела?')
+    button3 = telebot.types.KeyboardButton('Расскажи интересный факт')
+    kbrd.add(button1, button2, button3)
+    bot.send_message(message.chat.id, "Добрый день! Я небольшой эхо бот", reply_markup=kbrd)
 
-print(say_hello('Дима'))
-print(say_bye('Дима'))
+@bot.message_handler(commands=['help'])
+def start(message):
+    bot.send_message(message.chat.id, "Я пока умею только повторять за вами!")
+
+@bot.message_handler(commands=['goodbye'])
+def start(message):
+    bot.send_message(message.chat.id, "До свидания, было приятно пообщаться!")
+
+@bot.message_handler(content_types=['text'])
+def echo_message(message):
+    if message.text == 'Привет':
+        text = "И тебе привет!"
+    elif message.text == 'Расскажи интересный факт':
+        text = random.choice(facts)
+    elif message.text == 'Как дела?':
+        text = 'Хорошо, а у вас?'
+    else:
+        text = message.text
+    bot.reply_to(message, text)
+
+bot.polling(none_stop=True)
