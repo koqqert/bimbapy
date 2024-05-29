25fe3467b4f55ec85e0d6d1a6f9db8e54edc23a0
koqqert <koqqert@gmail.com>
2024-05-29 13:24:47 +0500

    добавил 1 функцию и поменял библеотеку для бота

--- project/telegram.py
+++ project/telegram.py
@@ -1,20 +1,47 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
 import os
+import sqlite3
+import telebot
 
-bot = Bot('6793400912:AAG_0zDEB36_IUoSCagzeiJAMBzk_vL7C-Y')
-dp = Dispatcher(bot=bot)
+bot = telebot.TeleBot('6793400912:AAG_0zDEB36_IUoSCagzeiJAMBzk_vL7C-Y')
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton('Журнал', web_app=types.WebAppInfo(url='https://koqqert.pythonanywhere.com/')))
-    await message.answer(f'Привет, {message.from_user.first_name}, напишите команду, которую хотите запустить.', reply_markup=markup)
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    web_info = telebot.types.WebAppInfo('https://koqqert.pythonanywhere.com/')
+    btn = telebot.types.KeyboardButton('Журнал', web_app=web_info)
+    markup.add(btn)
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}, напишите команду, которую хотите запустить.', reply_markup=markup)
 
-@dp.message_handler()
-async def answer(message: types.Message):
-    await message.reply(f"Я вас не понимаю.")
 
-if __name__ == '__main__':
-    asyncio.run(dp.start_polling())
+@bot.message_handler(commands=['add_day'])
+def ask_for_day(message):
+    bot.send_message(message.chat.id,'Какой столбец вы хотите добавить?(пример: д010924)')
+    bot.register_next_step_handler(message, process_day)
+def process_day(message):
+    day = message.text
+    bot.send_message(message.chat.id,f'{day}')
+
+    conn = sqlite3.connect('project\journal.db')
+    c = conn.cursor()
+
+    c.execute("PRAGMA table_info(journal)")
+    columns = [column[1] for column in c.fetchall()]
+
+    if day in columns:
+        bot.send_message(message.chat.id, f"Столбец '{day}' уже существует.")
+    else:
+        try:
+            c.execute(f"ALTER TABLE journal ADD COLUMN '{day}' TEXT NOT NULL DEFAULT '-'")
+            conn.commit()
+            bot.send_message(message.chat.id, f"Вы успешно добавили столбец '{day}' в таблицу.")
+        except sqlite3.OperationalError as e:
+            bot.send_message(message.chat.id, f"Ошибка при добавлении столбца: {e}")
+    
+    conn.close()
+
+@bot.message_handler()
+def answer(message):
+    bot.reply_to(message.chat.id,f"Я вас не понимаю.")
+
+bot.polling(none_stop=True)
