6327bcf7518e9581561cb86aeeed893e66a0ffdb
koqqert <koqqert@gmail.com>
2024-08-15 11:37:18 +0500

    ничего не понял, нужно больше вникать

--- learn_js/script.js
+++ learn_js/script.js
@@ -309,3 +309,31 @@ function sumInput(){
   }
   return numbers
 }
+
+//задание 28
+function camelize(str){
+  return str.split('-').map((word, index) =>
+    index == 0? word : word[0].toUpperCase() + word.slice(1)
+  ).join('');
+}
+//задание 29
+function filterRange(arr, a, b){
+  return arr.filter(value => (value <= a && value <= b));
+}
+//задание 30
+function filterRangeInPlace(arr, a, b) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+
+}
+//задание 31 
+let arr = [5, 2, 1, -10, 8];
+arr.sort((a, b) => b - a);
+alert( arr );
