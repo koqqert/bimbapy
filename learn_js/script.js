b58bdafd40266023c3a90abebd9f06337c09b379
koqqert <koqqert@gmail.com>
2024-09-08 10:20:38 +0500

    немного изучал классы

--- learn_js/script.js
+++ learn_js/script.js
@@ -457,22 +457,37 @@ function toCamelCase(str) {
 
 }
 
-let head = {
-  glasses: 1
-};
+class Clock {
+  constructor({template}){
+    this.template = template;
+  }
 
-let table = {
-  pen: 3
-  __proto__: head,
-};
+  render(){
+    let date = new Date();
 
-let bed = {
-  sheet: 1,
-  pillow: 2
-  __proto__: table,
-};
+    let hours = date.getHours();
+      if (hours < 10) hours = '0' + hours;
+  
+    let mins = date.getMinutes();
+      if (mins < 10) mins = '0' + mins;
+  
+    let secs = date.getSeconds();
+      if (secs < 10) secs = '0' + secs;
+    
+    let output = template
+        .replace('h', hours)
+        .replace('m', mins)
+        .replace('s', secs);
+  
+    console.log(output);
+    }
+  
+  stop() {
+    clearInterval(this.timer);
+  }
 
-let pockets = {
-  money: 2000,
-  __proto__: bed,
-};
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
