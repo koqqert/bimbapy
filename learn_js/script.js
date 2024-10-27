462b0e51bd28ac34db62349f684d82727bae7560
koqqert <koqqert@gmail.com>
2024-10-27 11:12:44 +0500

    практикуюсь

--- learn_js/script.js
+++ learn_js/script.js
@@ -683,3 +683,27 @@ function stop() {
   clearInterval(timerId);
   timerId = null;
 }
+
+let fieldonclick = function(event){ //field.onclick
+
+  let fieldCoords = this.getBoundingClientRect();
+  
+  let ballCoords = {
+    top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight/2,
+    left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth/2
+  }
+
+  if (ballCoords.top < 0) ballCoords.top = 0;
+  if (ballCoords.left < 0) ballCoords.left = 0;
+
+  if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+    ballCoords.left = field.clientWidth - ball.clientWidth;
+  }
+
+  if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+    ballCoords.top = field.clientHeight - ball.clientHeight;
+  }
+
+  ball.style.top = ballCoords.top + 'px';
+  ball.style.left = ballCoords.left + 'px';
+}
