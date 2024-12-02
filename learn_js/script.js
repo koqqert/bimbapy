0d47f90e4661bfe58fd33ebd5239354e8c32def4
koqqert <koqqert@gmail.com>
2024-12-02 20:57:39 +0500

    начал пробовать сам решать в задачи. больше практики

--- learn_js/script.js
+++ learn_js/script.js
@@ -717,3 +717,41 @@ function hexStringToRGB(hexString) {
 
   return {r: r, g: g, b: b};
 }
+
+let tooltip;
+
+document.onmouseover = function(event){
+  let anchorElem = event.target.closest('[data-tooltip]');
+
+  if (!anchorElem) return;
+
+  tooltip = showTooltip(anchorElem, anchorElem.dataset.tooltip);
+}
+
+document.onmouseout = function(event){
+  if (tooltip) {
+    tooltip.remove();
+    tooltip = null;
+  }
+}
+
+function showTooltip(anchorElem, html) {
+  let tooltipElem = document.createElement('div');
+  tooltipElem.className = 'tooltip';
+  tooltipElem.innerHTML = html;
+  document.body.append(tooltipElem);
+
+  let coords = getBoundingClientRect();
+  let left = coords.left + (anchorElem.offsetWidth - tooltipElem.offsetWidth) / 2;
+  if (left < 0) left = 0;
+
+  let top = coords.top - tooltipElem.offsetHeight - 5;
+
+  if (top < 0) {
+    top = coords.top + anchorElem.offsetHeight + 5;
+  }
+  tooltipElem.style.left = left + 'px';
+  tooltipElem.style.top = top + 'px';
+
+  return tooltipElem;
+}
