0d13bf2d048b4bd682c1364983d967718928845b
koqqert <koqqert@gmail.com>
2024-12-05 20:19:37 +0500

    пройдено 2 темы

--- learn_js/script.js
+++ learn_js/script.js
@@ -790,3 +790,25 @@ thumb.onmousedown = function(event) {
 thumb.ondragstart = function() {
   return false;
 };
+
+
+function runOkeys(func, ...codes) {
+  let pressed = new Set();
+
+  document.addEventListener('keydown', function(event) {
+    pressed.add(event.code); 
+  
+    for (let code of codes) {
+      if (!pressed.has(code)) {
+        return;
+      }
+    }
+
+    pressed.clear();
+    func();
+  });
+  
+  document.addEventListener('keyup', function(event) {
+    pressed.delete(event.code);
+  });
+}
